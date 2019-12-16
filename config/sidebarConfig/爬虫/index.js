d1ec8f614ac8a1c72580c85b8fd61ef22170d3a0
Hansiyuan131 <422295068@qq.com>
2019-12-16 22:58:48 +0800

    添加爬虫内容

--- config/sidebarConfig/爬虫/index.js
+++ config/sidebarConfig/爬虫/index.js
@@ -1,8 +1,15 @@
 module.exports = [
+    '',
+    {
+        title:'爬虫基础',
+        children:[
+            '002初识爬虫',
+            '003ChromeDevTools使用技巧',
+        ]
+    },
     {
         title:'爬虫实战',
         children:[
-            '',
             '001第一个Scrapy网络爬虫',
         ]
     }
